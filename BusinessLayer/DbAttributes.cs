0fb9559e281f852fec07c29664ce67c64f3d5dbd
Andromius <105814709+Andromius@users.noreply.github.com>
2023-04-21 02:30:26 +0200

    Added DbPrimaryKey attribute

--- BusinessLayer/DbAttributes.cs
+++ BusinessLayer/DbAttributes.cs
@@ -11,6 +11,14 @@
 
 	public class DbIgnore : Attribute { }
 	public class DbInner : Attribute { }
+	public class DbPrimaryKey : Attribute 
+	{
+		private bool autoIncrement;
+		public DbPrimaryKey(bool autoIncrement = true)
+		{
+			this.autoIncrement = autoIncrement;
+		}
+	}
 	public class DbForeignKey : Attribute { }
 	public class DbEntity : Attribute { }
 	public class DbConstructor : Attribute { }
