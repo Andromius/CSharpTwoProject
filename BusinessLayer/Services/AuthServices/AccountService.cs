c23ac7fc8c7e3a4d407021b7c0cdbdb789c974db
Andromius <105814709+Andromius@users.noreply.github.com>
2023-04-26 21:04:26 +0200

    Added AccountService and IAccountService

---
+++ BusinessLayer/Services/AuthServices/AccountService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.Identity;
+
+namespace BusinessLayer.Services.AuthServices
+{
+	public class AccountService : IAccountService
+	{
+		private IPasswordHasher _passwordHasher;
+		private IDataMappingService<User> _dataMapper;
+
+		public AccountService(IPasswordHasher passwordHasher, IDataMappingService<User> dataMapper)
+		{
+			_passwordHasher = passwordHasher;
+			_dataMapper = dataMapper;
+		}
+		public async Task<User?> Login(string email, string password)
+		{
+			User u = await _dataMapper.SelectWithCondition(new Dictionary<string, object>() { {"email", email } });
+			if (u == null)
+			{
+				return u;
+			}
+
+			PasswordVerificationResult result = _passwordHasher.VerifyHashedPassword(u.Password, password);
+			if(result != PasswordVerificationResult.Success)
+			{
+				return null;
+			}
+
+			return u;
+		}
+	}
+}
---
+++ BusinessLayer/Services/AuthServices/IAccountService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services.AuthServices
+{
+    public interface IAccountService
+    {
+        public Task<User?> Login(string email, string password);
+    }
+}
