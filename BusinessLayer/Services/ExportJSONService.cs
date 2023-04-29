543b6886c052818a8b5ea57fc4a3aed1f39065e4
Andromius <105814709+Andromius@users.noreply.github.com>
2023-04-29 16:07:50 +0200

    Added Export[JSON/XML]services

---
+++ BusinessLayer/Services/ExportJSONService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+	public class ExportJSONService<T> where T : DomainObject
+	{
+		private Stream _stream;
+		public ExportJSONService(Stream stream)
+		{
+			_stream = stream;
+		}
+		public void Export(List<T> elements)
+		{
+			if (elements.Count < 1)
+			{
+				_stream.Close();
+				_stream.Dispose();
+				return;
+			}
+			using (_stream)
+			{
+				using(StreamWriter sw = new StreamWriter(_stream))
+				{
+					sw.Write(JsonSerializer.Serialize(elements, new JsonSerializerOptions()
+					{
+							WriteIndented = true,
+							Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+					}));
+					sw.Flush();
+				}
+			}
+		}
+	}
+}
---
+++ BusinessLayer/Services/ExportXMLService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BusinessLayer.Services
+{
+	public class ExportXMLService<T>
+	{
+		private Stream _stream;
+		public ExportXMLService(Stream stream)
+		{
+			_stream = stream;
+		}
+		public void Export(List<T> elements)
+		{
+			if (elements.Count < 1)
+			{
+				_stream.Close();
+				_stream.Dispose();
+				return;
+			}
+			using (_stream)
+			{
+				DataContractSerializer xmlSerializer = new DataContractSerializer(typeof(List<T>));
+				xmlSerializer.WriteObject(_stream, elements);
+			}
+		}
+	}
+}
